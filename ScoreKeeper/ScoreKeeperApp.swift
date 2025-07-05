401314a561a744718efe57c180dadcf84d2ef09f
israeldail <israeldail2@gmail.com>
2025-07-05 19:56:55 -0400

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Israel Dail on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
