bf5700fd5a4d59eb832e3ec9ea1f527603ff0989
israeldail <israeldail2@gmail.com>
2025-07-05 20:12:52 -0400

    started player data type

--- ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var players: [String] = ["Israel", "Jzy", "Josh"]
+    @State private var scores: [Int] = [0,0,0]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(0..<players.count, id: \.description) { index in
+                TextField("Name", text: $players[index])
+                Stepper("\(scores[index])", value: $scores[index])
+            }
+            
+            Button("Add Player", systemImage: "plus") {
+                players.append("")
+                scores.append(0)
+            }
+            Spacer()
         }
         .padding()
     }
---
+++ ScoreKeeper/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  ScoreKeeper
+//
+//  Created by Israel Dail on 7/5/25.
+//
+
+struct Player {
+    var name: String
+    var score: Int
+    
+    
+}
